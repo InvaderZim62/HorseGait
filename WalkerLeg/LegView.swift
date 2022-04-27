dd4a4aa20edf4a0067b96f1dc0daac0a4a3387e3
Invader Zim <pwdelete@gmail.com>
2022-04-27 15:58:26 -0700

    Renamed "pivot" to "point" everywhere.

--- WalkerLeg/LegView.swift
+++ WalkerLeg/LegView.swift
@@ -8,12 +8,13 @@
 import UIKit
 
 struct LegConst {
+    static let pointRadius: CGFloat = 3
     static let crankLength: CGFloat = 30
     static let thighLength: CGFloat = 100
     static let kneeLength: CGFloat = 84
     static let kneeDepth: CGFloat = 69
     static let shinLength: CGFloat = 120
-    static let hamstringLength: CGFloat = 71.5
+    static let abDistance: CGFloat = 71.5
     static let hamstringAttach: CGFloat = kneeDepth
     static let footBottomLength: CGFloat = 167
     static let footTopLength: CGFloat = 135
@@ -53,78 +54,78 @@ class LegView: UIView {
     }
 
     override func draw(_ rect: CGRect) {
-        let pivotA =  CGPoint(x: centerX, y: centerY)
-        let pivotB = pivotA.offsetBy(dx: -LegConst.hamstringLength, dy: 0)
-        let pivotC = pivotA + CGPoint(x: LegConst.crankLength * cos(crankAngle), y: LegConst.crankLength * sin(crankAngle))
-        let lengthBC = pivotB.distance(to: pivotC)
-        let angleABC = pivotB.bearing(to: pivotC)
+        let pointA = CGPoint(x: centerX, y: centerY)
+        let pointB = pointA.offsetBy(dx: -LegConst.abDistance, dy: 0)
+        let pointC = pointA + CGPoint(x: LegConst.crankLength * cos(crankAngle), y: LegConst.crankLength * sin(crankAngle))
+        let lengthBC = pointB.distance(to: pointC)
+        let angleABC = pointB.bearing(to: pointC)
         let angleCBD = -acos((pow(lengthBC, 2) + pow(LegConst.kneeDepth, 2) - pow(LegConst.thighLength, 2)) / (2 * lengthBC * LegConst.kneeDepth))
         let angleABD = angleABC + angleCBD
-        let pivotD = pivotB + CGPoint(x: LegConst.kneeDepth * cos(angleABD), y: LegConst.kneeDepth * sin(angleABD))
+        let pointD = pointB + CGPoint(x: LegConst.kneeDepth * cos(angleABD), y: LegConst.kneeDepth * sin(angleABD))
         let angleDBE = -2 * asin(0.5 * LegConst.kneeLength / LegConst.kneeDepth)
         let angleABE = angleABD + angleDBE
-        let pivotE = pivotB + CGPoint(x: LegConst.kneeDepth * cos(angleABE), y: LegConst.kneeDepth * sin(angleABE))
+        let pointE = pointB + CGPoint(x: LegConst.kneeDepth * cos(angleABE), y: LegConst.kneeDepth * sin(angleABE))
         let angleCBF = acos((pow(lengthBC, 2) + pow(LegConst.hamstringAttach, 2) - pow(LegConst.thighLength, 2)) / (2 * lengthBC * LegConst.hamstringAttach))
         let angleABF = angleABC + angleCBF
-        let pivotF = pivotB + CGPoint(x: LegConst.hamstringAttach * cos(angleABF), y: LegConst.hamstringAttach * sin(angleABF))
-        let pivotG = pivotB + CGPoint(x: LegConst.shinLength * cos(angleABF), y: LegConst.shinLength * sin(angleABF))
-        let pivotH = pivotE + CGPoint(x: LegConst.shinLength * cos(angleABF), y: LegConst.shinLength * sin(angleABF))
+        let pointF = pointB + CGPoint(x: LegConst.hamstringAttach * cos(angleABF), y: LegConst.hamstringAttach * sin(angleABF))
+        let pointG = pointB + CGPoint(x: LegConst.shinLength * cos(angleABF), y: LegConst.shinLength * sin(angleABF))
+        let pointH = pointE + CGPoint(x: LegConst.shinLength * cos(angleABF), y: LegConst.shinLength * sin(angleABF))
         let angleHGT = -acos((pow(LegConst.kneeDepth, 2) + pow(LegConst.footTopLength, 2) - pow(LegConst.footBottomLength, 2)) / (2 * LegConst.kneeDepth * LegConst.footTopLength))
         let angleGHT = angleABE + angleHGT
-        let toePoint = pivotG + CGPoint(x: LegConst.footTopLength * cos(angleGHT), y: LegConst.footTopLength * sin(angleGHT))
+        let toePoint = pointG + CGPoint(x: LegConst.footTopLength * cos(angleGHT), y: LegConst.footTopLength * sin(angleGHT))
 
         UIColor.black.setFill()
         UIColor.lightGray.setFill()
 
         let crank = UIBezierPath()
-        crank.move(to: pivotA)
-        crank.addLine(to: pivotC)
+        crank.move(to: pointA)
+        crank.addLine(to: pointC)
         crank.stroke()
         
         let thigh = UIBezierPath()
-        thigh.move(to: pivotD)
-        thigh.addLine(to: pivotC)
-        thigh.addLine(to: pivotF)
+        thigh.move(to: pointD)
+        thigh.addLine(to: pointC)
+        thigh.addLine(to: pointF)
         thigh.stroke()
         
         let knee = UIBezierPath()
-        knee.move(to: pivotB)
-        knee.addLine(to: pivotD)
-        knee.addLine(to: pivotE)
+        knee.move(to: pointB)
+        knee.addLine(to: pointD)
+        knee.addLine(to: pointE)
         knee.close()
         knee.stroke()
         knee.fill()
         
         let shin = UIBezierPath()
-        shin.move(to: pivotE)
-        shin.addLine(to: pivotH)
+        shin.move(to: pointE)
+        shin.addLine(to: pointH)
         shin.stroke()
         
         let hamstring = UIBezierPath()
-        hamstring.move(to: pivotB)
-        hamstring.addLine(to: pivotG)
+        hamstring.move(to: pointB)
+        hamstring.addLine(to: pointG)
         hamstring.stroke()
         
         let foot = UIBezierPath()
-        foot.move(to: pivotG)
-        foot.addLine(to: pivotH)
+        foot.move(to: pointG)
+        foot.addLine(to: pointH)
         foot.addLine(to: toePoint)
         foot.close()
         foot.stroke()
         foot.fill()
         
-        drawCircleWith(center: pivotA)
-        drawCircleWith(center: pivotB)
-        drawCircleWith(center: pivotC)
-        drawCircleWith(center: pivotD)
-        drawCircleWith(center: pivotE)
-        drawCircleWith(center: pivotF)
-        drawCircleWith(center: pivotG)
-        drawCircleWith(center: pivotH)
+        drawCircleWith(center: pointA)
+        drawCircleWith(center: pointB)
+        drawCircleWith(center: pointC)
+        drawCircleWith(center: pointD)
+        drawCircleWith(center: pointE)
+        drawCircleWith(center: pointF)
+        drawCircleWith(center: pointG)
+        drawCircleWith(center: pointH)
     }
     
     private func drawCircleWith(center: CGPoint) {
-        let circle = UIBezierPath(arcCenter: center, radius: 4, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        let circle = UIBezierPath(arcCenter: center, radius: LegConst.pointRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
         circle.stroke()
     }
 }
