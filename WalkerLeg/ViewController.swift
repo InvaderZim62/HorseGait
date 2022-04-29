4358e28aa1455525907d10fc8cd197893bbe31c9
Invader Zim <pwdelete@gmail.com>
2022-04-29 09:29:51 -0700

    Added trot, canter, and gallop leg sequences.

--- WalkerLeg/ViewController.swift
+++ WalkerLeg/ViewController.swift
@@ -9,13 +9,17 @@ import UIKit
 
 struct Constants {
     static let frameTime = 0.05  // seconds
-    static let walkPeriod = 2.6  // seconds per step cycle
+    static let stridePeriod = 1.8  // seconds per four-step sequence
     static let viewWidth: CGFloat = 200
     static let torsoWidth: CGFloat = 10  // separation between front and rear leg views
     static let foregroundColor = #colorLiteral(red: 0.9998988509, green: 1, blue: 0.7175351977, alpha: 1)
     static let backgroundColor = #colorLiteral(red: 0.7775719762, green: 0.7785590291, blue: 0.5715256929, alpha: 1)
     static let pegColor = #colorLiteral(red: 0.7533841729, green: 0.7588498592, blue: 0.549562037, alpha: 1)
     static let pivotColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+}
+
+struct Gate {
+    var phase = [0.0, 1.5, 1.0, 0.5]  // left rear, left front, right rear, right front
 }
 
 class ViewController: UIViewController {
@@ -26,6 +30,11 @@ class ViewController: UIViewController {
     let rightFrontLegView = LegView()
     var simulationTimer = Timer()
     var rotationAngle = -1.5 { didSet { updateViewFromModel() } } // 0 to right, positive clockwise in radians
+    var gait = Gate()
+    let walk = Gate(phase: [0.0, 0.5, 1.0, 1.5])
+    let trot = Gate(phase: [0.0, 1.0, 1.0, 0.0])
+    let canter = Gate(phase: [0.0, 0.3, 0.3, 0.9])
+    let gallop = Gate(phase: [0.0, 0.6, 0.3, 0.9])
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -67,14 +76,15 @@ class ViewController: UIViewController {
         rightFrontLegView.layer.transform = CATransform3DMakeScale(-1, 1, 1)  // flip vertically
         view.addSubview(rightFrontLegView)
 
+        gait = canter
         startSimulation()
     }
-
+    
     private func updateViewFromModel() {
-        leftRearLegView.crankAngle = rotationAngle + Double.pi
-        leftFrontLegView.crankAngle = -(rotationAngle - 3 * Double.pi / 2)
-        rightRearLegView.crankAngle = rotationAngle
-        rightFrontLegView.crankAngle = -(rotationAngle - Double.pi / 2)
+        leftRearLegView.crankAngle = rotationAngle - gait.phase[0] * Double.pi
+        leftFrontLegView.crankAngle = -(rotationAngle - gait.phase[1] * Double.pi)
+        rightRearLegView.crankAngle = rotationAngle - gait.phase[2] * Double.pi
+        rightFrontLegView.crankAngle = -(rotationAngle - gait.phase[3] * Double.pi)
     }
 
     private func startSimulation() {
@@ -84,7 +94,7 @@ class ViewController: UIViewController {
     }
     
     @objc func updateSimulation() {
-        let deltaAngle = 2 * Double.pi / Constants.walkPeriod * Constants.frameTime
+        let deltaAngle = 2 * Double.pi / Constants.stridePeriod * Constants.frameTime
         rotationAngle = (rotationAngle + deltaAngle).truncatingRemainder(dividingBy: 2 * Double.pi)
     }
 }
